5342d2c57541fda6f6d75b9a2226b0cab939edee
steingartpeter <steingartp.2@gmail.com>
2020-06-18 21:07:02 +0200

    SETUP_02

---
+++ JS/lib/mainApp.js
@@ -0,0 +1,64 @@
+//<M>
+//×-
+//@-FILENÉV   : BAREBONE_02 - mainApp.js-@
+//@-SZERZŐ    : AX07057-@
+//@-LÉTREHOZVA: 2020-06-18.-@
+//@-FÜGGŐSÉGEK:
+//×-
+// @-- RQRD_FILE01.js-@
+// @-- RQRD_FILE02.js-@
+// @-- RQRD_FILE03.js-@
+// @-- RQRD_FILE04.js-@
+//-@
+//-×
+//-@
+//@-LEÍRÁS    :
+//Ez az javascript file készült arra a feladatra, hogy ...
+//@-MÓDOSÍTÁSOK :
+//×-
+// @-- ... -@
+//-×
+//-×
+//</M>
+
+
+//<nn>
+// A teljes javascrip alkalmazást tartlamazó objektum.<br/>
+// Érdemes neki olyan nevet adni ami az aktuális projektet írja le.<br/>
+// Ennek az objektumnak az a szerepe, hogy a kódunkat elkülönítse a globális névtértől, 
+// így az esetleges névütközéseknek lejét vehetjük.
+//</nn>
+var mainApp = mainApp || {};
+
+//<nn>
+// A document.ready függvényben a jQueryn keresztül meghívhatjuk az inicializációs kódunkat.
+//</nn>
+$( init);
+
+
+function init(){
+	//<SF>
+	// 2020-06-18<br>
+	// Az incializációs függvény, ami meghívódik a jQuery miatt a lap ready() fügvényével.<br>
+	// PARAMÉTEREK:
+	//×-
+	// @- nincsenek paraméeterei -@
+	//-×
+	//MÓDOSÍTÁSOK:
+	//×-
+	// @-- ... -@
+	//-×
+	//</SF>	
+	
+	
+	console.log("Inicializítion done ...");
+}
+
+//+-----------------------------------------------------------------------------+
+//|¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤|
+//|                               SEGÉD FÜGGVÉNYEK                              |
+//|¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤|
+//+-----------------------------------------------------------------------------+
+
+
+
